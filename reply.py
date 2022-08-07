6cf529acfbb4fa584361ea47be2cba4e22d4e04c
Harmonyano <Harmonyanop@gmail.com>
2022-08-06 23:17:01 -0400

    First Message Commit

---
+++ reply.py
@@ -0,0 +1,39 @@
+warmth = [
+	# https://www.reddit.com/r/Cornell/comments/ukzuvt/will_cornell_rescind_me/i7tbjpg/
+	"i work in admissions and they had this exact file up while we were going through who got rescinded yesterday :( sucks to suck but i guess ur dumb",
+	
+	# https://www.reddit.com/r/Cornell/comments/ukzuvt/will_cornell_rescind_me/i7scd5k/
+	"Yes you are a dumbass and don't deserve to come here",
+	
+	# https://www.reddit.com/r/Cornell/comments/ukzuvt/will_cornell_rescind_me/i7usx6y/
+	"You will be rescinded. Bye.",
+
+	# https://www.reddit.com/r/Cornell/comments/8mmzzv/update_rescinded_from_cornell/dzqbpak/
+	"you deserve it",
+
+	# https://www.reddit.com/r/Cornell/comments/uoln54/the_hated_will_i_be_rescinded_post/i8ff6tz/
+	"Hate to break it to you but you'll almost certainly be rescinded. In fact, I will personally see to it that you get rescinded.", 
+	
+	# https://www.reddit.com/r/Cornell/comments/u7m5en/rescinded/i5g4sij/
+	"They actually rescind people who do too good in their senior year, because the true gigachads don't play the AP/honors games. I suggest absolutely tanking all those useless honors and AP classes.",
+
+	# https://www.reddit.com/r/Cornell/comments/u7m5en/rescinded/i5hos6l/
+	"No you’ll go to jail, aka the Clark basement.",
+
+	# https://www.reddit.com/r/Cornell/comments/u7m5en/rescinded/i5gxe7p/
+	"Bad news, Mother Martha hates B students. Once she sees your transcript you’re gonna be rescinded, excommunicated, then dumped in the Ithaca wilderness to be hunted for sport. ",
+
+	# https://www.reddit.com/r/Cornell/comments/u7m5en/rescinded/i5ilas3/
+	"Does anyone remember the pitiful applicant who didn't recognize Martha at Ithaca Farmers Market?",
+
+	# https://www.reddit.com/r/Cornell/comments/u7m5en/rescinded/i5jfpm3/
+	"You dumbasses. You only lose your offer if you can't finish your HS diploma.",
+
+	# https://www.reddit.com/r/Cornell/comments/ucquk6/im_getting_massive_senioritis_what_are_my_chances/i6cb8lr/
+	"I say this with all the love and care in the world—shut the fuck up.",
+
+	# https://www.reddit.com/r/Cornell/comments/ucquk6/im_getting_massive_senioritis_what_are_my_chances/i6etimy/
+	"i just called up the admissions office bro, it’s over for you.",
+
+	"Yes.", "People like you don't deserve Cornell", "Yes your fucked.",
+]
